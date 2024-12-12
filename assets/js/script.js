c7b00690e816a2217d6c2896c5c7cb981d71eac0
Tarek Hossain <devlopertarek433@gmail.com>
2024-12-12 10:08:48 +0600

    checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -269,18 +269,20 @@
        });
        
        
-       
+       $('html, body').animate({
+        scrollTop: $(".middle").offset().top
+     }, 2000);
        
        // scrollToTop
-       $.scrollup({
-              scrollName: 'scrollup', // Element ID
+       $.scrollUp({
+              scrollName: 'scrollUp', // Element ID
               topDistance: '300', // Distance from top before showing element (px)
               topSpeed: 300, // Speed back to top (ms)
               animation: 'fade', // Fade, slide, none
               animationInSpeed: 200, // Animation in speed (ms)
               animationOutSpeed: 200, // Animation out speed (ms)
-              scrollText: '<i class="fas fa-angle-down"></i>', // Text for element
-              activeOverlay: true, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+              scrollText:' <i class="fas fa-angle-up"></i>', // Text for element
+              activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
        });
        
        // WOW active
